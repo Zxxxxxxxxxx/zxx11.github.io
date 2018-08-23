0c4edc07e1936c652a3e35e38db916b69b124bb4
zxx <1047655911@qq.com>
2018-08-23 10:57:44 +0800

    匿名函数和闭包理解

--- js/index.js
+++ js/index.js
@@ -1,70 +1,64 @@
-// 固定每张图片出于的位子和状态
+
+// 首先可以将整体封装一个匿名函数
+(function(){
+    // 固定每张图片出于的位子和状态
 var states = [{
-        ZIndex: 1,
-        width: 120,
-        height: 150,
-        top: 69,
-        left: 134,
-        opac: 0.2
-    },
-    {
-        ZIndex: 2,
-        width: 130,
-        height: 170,
-        top: 59,
-        left: 0,
-        opac: 0.5
-    },
-    {
-        ZIndex: 3,
-        width: 170,
-        height: 218,
-        top: 24,
-        left: 110,
-        opac: 0.7
-    },
-    {
-        ZIndex: 4,
-        width: 218,
-        height: 288,
-        top: 0,
-        left: 263,
-        opac: 1
-    },
-    {
-        ZIndex: 3,
-        width: 170,
-        height: 218,
-        top: 24,
-        left: 470,
-        opac: 0.7
-    },
-    {
-        ZIndex: 2,
-        width: 130,
-        height: 170,
-        top: 59,
-        left: 620,
-        opac: 0.5
-    },
-    {
-        ZIndex: 1,
-        width: 120,
-        height: 150,
-        top: 69,
-        left: 500,
-        opac: 0.2
-    },
+    ZIndex: 1,
+    width: 120,
+    height: 150,
+    top: 69,
+    left: 134,
+    opac: 0.2
+},
+{
+    ZIndex: 2,
+    width: 130,
+    height: 170,
+    top: 59,
+    left: 0,
+    opac: 0.5
+},
+{
+    ZIndex: 3,
+    width: 170,
+    height: 218,
+    top: 24,
+    left: 110,
+    opac: 0.7
+},
+{
+    ZIndex: 4,
+    width: 218,
+    height: 288,
+    top: 0,
+    left: 263,
+    opac: 1
+},
+{
+    ZIndex: 3,
+    width: 170,
+    height: 218,
+    top: 24,
+    left: 470,
+    opac: 0.7
+},
+{
+    ZIndex: 2,
+    width: 130,
+    height: 170,
+    top: 59,
+    left: 620,
+    opac: 0.5
+},
+{
+    ZIndex: 1,
+    width: 120,
+    height: 150,
+    top: 69,
+    left: 500,
+    opac: 0.2
+},
 ];
-
-
-
-
-
-
-
-
-
 
 // 将状态和位置赋给li
 var lis = $('#box li');
@@ -72,14 +66,6 @@ var lis = $('#box li');
 function move() {
     lis.each(function (index, ele) {
         var state = states[index]
-        // $(ele).css({
-        //     'z-index':state.ZIndex,
-        //     'width':state.width,
-        //     'height':state.height,
-        //     'top':state.top,
-        //     'left':state.left,
-        //     'opac':state.opac
-        // })
         $(ele).css('z-index', state.ZIndex).finish().animate(state, 1000).find('img').css('opacity', state.opac)
 })
 }
@@ -114,6 +100,20 @@ $('#box section').add('#box li').hover(function(){
 },function(){
     autoplay()
 })
+})()
+
+//变量的作用域问题
+//1.全局域[window]   2.函数域[function]
+// 1.全局域:  在页面被打开之后到页面关闭之前始终都是存在的
+// 2.函数域:  存在函数域被调用的一瞬间，(也不一定，考虑闭包的存在) 
+
+// 闭包: 可以保留函数的作用域(所以move可以使用当前自运行函数中的states
+// 闭包产生的必要条件: 函数里面套函数(内层的函数要使用外部函数的变量)
+
+// 全局变量会产生闭包么？
+// 不会,因为全局变量存在全局域
+
+
 
 // 封装为插件，能够使得只要使用这个插件，就能被重复的使用效果，会产生什么样的问题?
 // 1.插件中最好不要使用id，原因:插件是为了能够被重复使用，也就是说在一个页面上可能会使用重复，会造成页面的冲突，并且ID具有唯一性的特性.
